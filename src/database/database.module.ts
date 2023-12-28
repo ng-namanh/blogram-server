e4970811e1b496ffecf6debe4eccf07950507477
nna-n5Ember13 <namanh200313@gmail.com>
2023-12-29 02:24:41 +0700

    ↩ revert: rollback to local db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'db',
+        host: 'localhost',
         port: configService.get('POSTGRES_PORT'),
         username: configService.get('POSTGRES_USERNAME'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DATABASE'),
-        entities: [],
+        entities: [User],
         synchronize: configService.getOrThrow('POSTGRES_SYNCHRONZIZE'),
         autoLoadEntities: true,
       }),
+      inject: [ConfigService],
     }),
   ],
 })
