3583123636c9d691b26ca4cf920632cefc0b3a9b
nna-n5Ember13 <namanh200313@gmail.com>
2024-01-23 23:38:12 +0700

    ✨ feat: post controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,14 +1,31 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { Services, Routes } from 'src/utils/constants';
 import { IPostService } from './post';
 import { CreatePostDto } from './dto/post.dto';
+import { ReturnMessage } from 'src/utils/types';
+import { JwtAuthGuard } from 'src/auth/utils/guard.auth';
 
 @Controller(Routes.POST)
 export class PostController {
   constructor(@Inject(Services.POST) private postService: IPostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('/new')
-  createPost(@Body() postDto: CreatePostDto) {
-    return this.postService.createPost(postDto);
+  createPost(@Body() postDto: CreatePostDto, @Request() req) {
+    this.postService.createPost(postDto);
+    console.log(req);
+
+    return {
+      user: req.user,
+      success: true,
+      message: 'Post successfully published',
+    } as ReturnMessage;
   }
 }
